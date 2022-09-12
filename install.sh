a45a9f3563421e48531b6d74a4eb429b35772387
Your Name <you@example.com>
2022-09-12 08:48:38 +0000

    updated

--- install.sh
+++ install.sh
@@ -6,4 +6,8 @@ make install
 make clean
 cd ..
 make
-cp -u ../talib/talib_install/lib/libta_lib.so.0 ./
+cp -u ./talib/talib_install/lib/libta_lib.so.0 ./
+echo "  "  >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=$(pwd)/talib/talib_install/lib/:$LD_LIBRARY_PATH" >> ~/.bashrc
+echo "  "  >> ~/.bashrc
+source ~/.bashrc
